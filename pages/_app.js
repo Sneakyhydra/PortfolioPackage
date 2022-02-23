94cd37aeb7da1e2389de2d4086a1fbcab8193518
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2022-02-23 12:02:18 +0530

    Added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<div>
+			<Head>
+				<title>Dhruv Rishishwar</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			</Head>
+			<Component {...pageProps} />
+		</div>
+	);
 };
 
 export default MyApp;
