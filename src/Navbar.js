1110d62f516b89ede6aa5506fa72d3aa590b80bc
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2022-03-21 19:05:25 +0530

    Added navbar and theme toggle button

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,39 @@
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+import styles from '../styles/Navbar.module.css';
+
 const Navbar = ({ theme, setTheme }) => {
-	return <nav></nav>;
+	const changeTheme = () => {
+		if (theme === 'theme-light') {
+			setTheme('theme-dark');
+		} else {
+			setTheme('theme-light');
+		}
+	};
+
+	let icon =
+		theme === 'theme-light' ? (
+			<button onClick={changeTheme} className={styles.icon}>
+				<DarkModeIcon fontSize='large' />
+			</button>
+		) : (
+			<button onClick={changeTheme} className={styles.icon}>
+				<LightModeIcon fontSize='large' />
+			</button>
+		);
+
+	return (
+		<nav
+			className={
+				(theme === 'theme-dark' ? 'theme-dark' : 'theme-light') +
+				' ' +
+				styles.navbar
+			}
+		>
+			{icon}
+		</nav>
+	);
 };
 
 export default Navbar;
