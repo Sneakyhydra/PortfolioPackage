07397b6e3dd707a717b50bdf228656cd92b457f6
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2022-03-23 00:13:24 +0530

    Added dynamic text

--- components/Landing.js
+++ components/Landing.js
@@ -1,3 +1,5 @@
+import ReactTyped from 'react-typed';
+
 // Css
 import styles from '../styles/Landing.module.css';
 
@@ -5,9 +7,26 @@ import styles from '../styles/Landing.module.css';
 import PropTypes from 'prop-types';
 
 const Landing = ({ theme }) => {
+	const subtitles = [
+		'Full-stack developer 🌐',
+		'Exploring web 3',
+		'Just getting started 📈',
+	];
+
 	return (
-		<div className={styles.container + ' ' + theme}>
-			<h1 className='text-3xl'>Hi, I&#39;m Dhruv</h1>
+		<div
+			className={
+				styles.container +
+				' ' +
+				theme +
+				' flex flex-col gap-3 justify-center pl-4'
+			}
+		>
+			<h1 className='text-6xl font-light'>Hi, I&#39;m Dhruv</h1>
+			<h2 className='text-xl p-1'>
+				<ReactTyped strings={subtitles} typeSpeed={40} backSpeed={50} loop />
+			</h2>
+			<div className='flex flex-row gap-2'></div>
 		</div>
 	);
 };
