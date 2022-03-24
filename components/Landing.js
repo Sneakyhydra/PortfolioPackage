e40aac50b596dfe0b686bc00bdda91eeb449dc74
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2022-03-24 13:38:27 +0530

    Removed react-typed

--- components/Landing.js
+++ components/Landing.js
@@ -1,5 +1,5 @@
 // React Typed for dynamic text
-import ReactTyped from 'react-typed';
+// import ReactTyped from 'react-typed';
 
 // Components
 import Icon from './Icon';
@@ -47,9 +47,9 @@ const Landing = ({ theme }) => {
 			}
 		>
 			<h1 className='text-6xl font-light'>Hi, I&#39;m Dhruv</h1>
-			<h2 className='text-xl p-1'>
+			{/* <h2 className='text-xl p-1'>
 				<ReactTyped strings={subtitles} typeSpeed={40} backSpeed={50} loop />
-			</h2>
+			</h2> */}
 			<div className='flex flex-row gap-2 h-16'>
 				{professionalDetails.map(({ alt, icon, link }, idx) => (
 					<Icon key={idx} alt={alt} icon={icon} link={link} />
