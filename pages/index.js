2c16ef22adca75e58e74f2c6be7f7842bd67310c
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2022-03-24 13:34:40 +0530

    Removed other components

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import { useState, useEffect, Fragment } from 'react';
 // Components
 import Navbar from '../components/Navbar';
 import Landing from '../components/Landing';
-import Experience from '../components/Experience';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
-import Description from '../components/Description';
-import Contact from '../components/Contact';
-import Preloader from '../components/Preloader';
+// import Experience from '../components/Experience';
+// import Projects from '../components/Projects';
+// import Skills from '../components/Skills';
+// import Description from '../components/Description';
+// import Contact from '../components/Contact';
+// import Preloader from '../components/Preloader';
 
 const Home = () => {
 	// Theme
@@ -60,11 +60,11 @@ const Home = () => {
 		<Fragment>
 			<Navbar theme={theme} setTheme={setTheme} />
 			<Landing theme={theme} />
-			<Description theme={theme} />
+			{/* <Description theme={theme} />
 			<Skills theme={theme} />
 			<Projects theme={theme} />
 			<Experience theme={theme} />
-			<Contact theme={theme} />
+			<Contact theme={theme} /> */}
 		</Fragment>
 	);
 };
